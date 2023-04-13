800e25ec6a283063cb90c0781c11253714175e70
Ayush Gupta <ayu8058020@gmail.com>
2023-04-13 13:17:27 +0530

    final commit

--- src/components/employeeDashboard/dashboardNavigation.vue
+++ src/components/employeeDashboard/dashboardNavigation.vue
@@ -3,11 +3,9 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-<<<<<<< HEAD
-      <a class="navbar-brand" href="/Dashboard">Dashboard</a>
-=======
+
       <router-link to="/Dashboard">Dashboard</router-link>
->>>>>>> Ayush
+
       <button
         class="navbar-toggler"
         type="button"
@@ -18,15 +16,13 @@
         aria-label="Toggle navigation"
       >
         <span class="navbar-toggler-icon"></span>
-<<<<<<< HEAD
-=======
-        
->>>>>>> Ayush
+
+
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-<<<<<<< HEAD
+
             <a class="nav-link active" href="/EmployeeList">Employees List</a>
           </li>
           <li class="nav-item">
@@ -40,7 +36,7 @@
               class="nav-link active"
               href="/Profile"
               >Profile</a>
-=======
+
             <router-link to="/EmployeeList">Employee List</router-link>
           </li>
           <li class="nav-item">
@@ -51,7 +47,7 @@
           </li>
           <li class="nav-item">
               <router-link to="/Profile">Profile</router-link>
->>>>>>> Ayush
+
           </li>
         </ul>
       </div>
