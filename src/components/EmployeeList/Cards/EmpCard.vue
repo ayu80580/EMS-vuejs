91e6bde06c0389251f8d884c7ba144ed86fce20f
fillwithgreencolor <aniket620aniket@gmail.com>
2023-04-14 10:15:17 +0530

    Added leaveRequest, EmployeeList component to the backend

--- src/components/EmployeeList/Cards/EmpCard.vue
+++ src/components/EmployeeList/Cards/EmpCard.vue
@@ -1,56 +1,51 @@
 <template>
-        <tr>
-            <th scope="row">{{ id }}</th>
-            <!-- <td>
+    <tr>
+        <th scope="row">{{ id }}</th>
+        <!-- <td>
                 <img src="" alt="">
             </td> -->
-            <td>
-                <a class="text-decoration-none" @click.prevent="" href="">{{ name }}</a>
-            </td>
-            <td>
-                {{ phone }}
-            </td>
-            <td>
-                {{ email }}
-            </td>
-            <td>
-                {{ role }}
-            </td>
-            <div class="row">
-                <div class="col-sm-7">
-                    <div class="form-group">
-                        <select class="form-select mb-3" 
-                        aria-label=".form-select-lg example" 
-                        v-model="selected" 
-                        >
-                            <option value="1">Active</option>
-                            <option value="0">Inactive</option>
-                        </select>
-                    </div>
-                </div>
+        <td>
+            <a class="text-decoration-none" @click.prevent="" href="">{{ name }}</a>
+        </td>
+        <td>
+            {{ phone }}
+        </td>
+        <td>
+            {{ email }}
+        </td>
+        <td>
+            {{ role }}
+        </td>
+        <td>
+            <div class="form-group">
+                <select class="form-select mb-3" aria-label=".form-select-lg example" v-model="selected">
+                    <option value="1">Active</option>
+                    <option value="0">Inactive</option>
+                </select>
             </div>
-            <td>
-                {{ jod }}
-            </td>
-            <td>
-                <button type="button" class="btn btn-outline-primary" @click.prevent="saveStatus" >Save</button>
-            </td>
-        </tr>
+        </td>
+        <td>
+            {{ jod }}
+        </td>
+        <td>
+            <button type="button" class="btn btn-outline-primary" @click.prevent="saveStatus">Save</button>
+        </td>
+    </tr>
 </template>
 
 <script>
 export default {
-    
-    props: ['id','name', 'phone', 'email','role', 'status', 'jod',],
+
+    props: ['id', 'name', 'phone', 'email', 'role', 'status', 'jod',],
     data() {
         return {
-            selected:1,
+            selected: this.status
         }
     }
-    
+
+
+
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
